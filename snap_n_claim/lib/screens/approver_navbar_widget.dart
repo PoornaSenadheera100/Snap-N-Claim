f67c5f00d5c679d104c61cdeeef5a7cd7f6041bc
SanuthiVihansa <subasofts@gmail.com>
2023-09-18 22:22:16 +0530

    Commit NavBar modifications, Adding an avatar picture

--- snap_n_claim/lib/screens/approver_navbar_widget.dart
+++ snap_n_claim/lib/screens/approver_navbar_widget.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+
 class ApproverNavBar extends StatelessWidget {
   const ApproverNavBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child:ListView(
-        children: [
-          UserAccountsDrawerHeader(accountName: Text('Head of department'), accountEmail: Text('Approver@gmail.com'),currentAccountPicture: CircleAvatar(child: ClipOval(child: Image.asset('assets/avatarpic.jpg'),),),)
-        ],
-      )
-    );
+        child: ListView(
+          padding: EdgeInsets.zero,
+      children: [
+        UserAccountsDrawerHeader(
+          accountName: Text('Head of department'),
+          accountEmail: Text('Approver@gmail.com'),
+          currentAccountPicture: CircleAvatar(
+            child: ClipOval(
+              child: Image.asset(
+                'assets/avatarpic.jpg',
+                width: 90,
+                height: 90,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.blueAccent,
+            image: new DecorationImage(
+              image:new AssetImage('assets/useravatarimage.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
