a8bd1eafe2325f1cdc06264c95929d0312fe9f07
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-09-25 16:03:58 +0530

    Added navigation to report selection screen

--- snap_n_claim/lib/screens/finance_admin_menu_drawer.dart
+++ snap_n_claim/lib/screens/finance_admin_menu_drawer.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:snap_n_claim/screens/finance_admin_home_screen.dart';
+import 'package:snap_n_claim/screens/reports_selection_screen.dart';
 
 class FinanceAdminMenuDrawer extends StatelessWidget {
-   const FinanceAdminMenuDrawer(this._width, this._height, {super.key});
+  const FinanceAdminMenuDrawer(this._width, this._height, this.currentPage,
+      {super.key});
 
   final double _width;
-   final double _height;
+  final double _height;
+  final String currentPage;
+
+  void _onTapPendingClaimsBtn(BuildContext context) {
+    Navigator.of(context).pop();
+    if (currentPage != "Finance Pending Claims") {
+      Navigator.pushReplacement(
+        context,
+        PageTransition(
+          child: FinanceAdminHomeScreen(_width, _height),
+          type: PageTransitionType.rightToLeft,
+          alignment: Alignment.center,
+          isIos: true,
+          duration: const Duration(seconds: 1),
+        ),
+      );
+    }
+  }
+
+  void _onTapReportsBtn(BuildContext context) {
+    Navigator.of(context).pop();
+    if (currentPage != "Reports") {
+      Navigator.pushReplacement(
+        context,
+        PageTransition(
+          child: ReportsSelectionScreen(_width, _height),
+          type: PageTransitionType.rightToLeft,
+          alignment: Alignment.center,
+          isIos: true,
+          duration: const Duration(seconds: 1),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Drawer(
         child: ListView(
       children: [
@@ -22,18 +58,68 @@ class FinanceAdminMenuDrawer extends StatelessWidget {
           ),
         ),
         Padding(
-          padding:  EdgeInsets.symmetric(horizontal: _width/19.63636363636364), // 20
-          child: ElevatedButton(onPressed: () {}, child: const Text("Pending Claims")),
+          padding: EdgeInsets.symmetric(
+            horizontal: _width / 19.63636363636364,
+          ), // 20
+          child: ElevatedButton(
+            onPressed: () {
+              _onTapPendingClaimsBtn(context);
+            },
+            child: const Text("Pending Claims"),
+          ),
         ),
         Padding(
-          padding:  EdgeInsets.symmetric(horizontal: _width/19.63636363636364), // 20
-          child: ElevatedButton(onPressed: () {}, child: const Text("Reporting and Analytics")),
+          padding: EdgeInsets.symmetric(
+            horizontal: _width / 19.63636363636364,
+          ), // 20
+          child: ElevatedButton(
+            onPressed: () {
+              _onTapReportsBtn(context);
+            },
+            child: const Text("Reports"),
+          ),
         ),
         Padding(
-          padding:  EdgeInsets.symmetric(horizontal: _width/19.63636363636364), // 20
-          child: ElevatedButton(onPressed: () {}, child: const Text("My Claims")),
+          padding: EdgeInsets.symmetric(
+            horizontal: _width / 19.63636363636364,
+          ), // 20
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text("Budget Configurations"),
+          ),
         ),
-
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: _width / 19.63636363636364,
+          ), // 20
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text("User Configurations"),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: _width / 19.63636363636364,
+          ), // 20
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text("My Claims"),
+          ),
+        ),
+        Divider(thickness: 5, indent: 20, endIndent: 20,),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: _width / 19.63636363636364,
+          ), // 20
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith(
+                      (states) => Colors.red),
+            ),
+            child: const Text("Sign out"),
+          ),
+        ),
       ],
     ));
   }
