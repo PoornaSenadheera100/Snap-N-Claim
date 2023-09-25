b6aebea5df7dbbbc7eebcbe7e55bb844bba67e51
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-09-25 16:41:23 +0530

    Added new finance_admin_reports_selection_screen.dart

--- snap_n_claim/lib/screens/finance_admin_reports_selection_screen.dart
+++ snap_n_claim/lib/screens/finance_admin_reports_selection_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
+import 'finance_admin_budget_allocation_selection_screen.dart';
 import 'finance_admin_menu_drawer.dart';
+import 'finance_admin_reporting_screen.dart';
 
 class FinanceAdminReportsSelectionScreen extends StatelessWidget {
-   FinanceAdminReportsSelectionScreen(this._width, this._height, {super.key});
+  FinanceAdminReportsSelectionScreen(this._width, this._height, {super.key});
 
   double _width;
   double _height;
+
+  void _onTapReportingAndAnalyticsBtn(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      PageTransition(
+        child: FinanceAdminReportingScreen(_width, _height),
+        type: PageTransitionType.rightToLeft,
+        alignment: Alignment.center,
+        isIos: true,
+        duration: const Duration(seconds: 1),
+      ),
+    );
+  }
+
+  void _onTapViewClaimStatusBtn(BuildContext context) {}
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +33,23 @@ class FinanceAdminReportsSelectionScreen extends StatelessWidget {
         title: Text("Reports"),
       ),
       drawer: FinanceAdminMenuDrawer(_width, _height, "Reports"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  _onTapReportingAndAnalyticsBtn(context);
+                },
+                child: Text("Reporting and Analytics")),
+            ElevatedButton(
+                onPressed: () {
+                  _onTapViewClaimStatusBtn(context);
+                },
+                child: Text("View Claim Status")),
+          ],
+        ),
+      ),
     );
   }
 }
