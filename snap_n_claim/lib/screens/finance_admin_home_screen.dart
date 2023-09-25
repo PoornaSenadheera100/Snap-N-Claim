305b3c4144b866b92a84005653583212f15e5e16
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-09-25 16:40:37 +0530

    Added new finance admin home

--- snap_n_claim/lib/screens/finance_admin_home_screen.dart
+++ snap_n_claim/lib/screens/finance_admin_home_screen.dart
@@ -1,49 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:snap_n_claim/screens/finance_admin_budget_allocation_selection_screen.dart';
-import 'package:snap_n_claim/screens/finance_admin_reporting_screen.dart';
+import 'package:snap_n_claim/screens/finance_admin_menu_drawer.dart';
 
-class FinanceAdminHomeScreen extends StatelessWidget {
+class FinanceAdminHomeScreen extends StatefulWidget {
   FinanceAdminHomeScreen(this._width, this._height, {super.key});
 
   double _width;
   double _height;
 
-  void _onTapReportingAndAnalyticsBtn(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(
-        builder: (BuildContext context) => FinanceAdminReportingScreen(_width, _height)));
-  }
+  @override
+  State<FinanceAdminHomeScreen> createState() => _FinanceAdminHomeScreenState();
+}
 
-  void _onTapBudgetAllocationBtn(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(
-        builder: (BuildContext context) =>
-            FinanceAdminBudgetAllocationSelectionScreen(_width, _height)));
-  }
-
+class _FinanceAdminHomeScreenState extends State<FinanceAdminHomeScreen> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Welcome !"),
+        title: Text("Finance Pending Claims"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  _onTapReportingAndAnalyticsBtn(context);
-                },
-                child: Text("Reporting and Analytics")),
-            ElevatedButton(
-                onPressed: () {
-                  _onTapBudgetAllocationBtn(context);
-                },
-                child: Text("Budget Allocation")),
-            ElevatedButton(
-                onPressed: () {}, child: Text("Account Creation & Management")),
-          ],
-        ),
-      ),
+      drawer: FinanceAdminMenuDrawer(widget._width, widget._height, "Finance Pending Claims"),
     );
   }
 }
