2c19a18e9baa74ddab086da1cf445203220c0a48
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-09-25 15:20:28 +0530

    Align buttons

--- snap_n_claim/lib/screens/finance_admin_menu_drawer.dart
+++ snap_n_claim/lib/screens/finance_admin_menu_drawer.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 
 class FinanceAdminMenuDrawer extends StatelessWidget {
-  const FinanceAdminMenuDrawer({super.key});
+   const FinanceAdminMenuDrawer(this._width, this._height, {super.key});
+
+  final double _width;
+   final double _height;
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-        child:ListView(
-          children: [
-            UserAccountsDrawerHeader(accountName: Text('Finance Administrator'), accountEmail: Text('finadmin@spsh.lk'),currentAccountPicture: CircleAvatar(child: ClipOval(child: Image.asset('assets/avatarpic.jpg'),),),)
-          ],
-        )
-    );
 
+    return Drawer(
+        child: ListView(
+      children: [
+        UserAccountsDrawerHeader(
+          accountName: const Text('Finance Administrator'),
+          accountEmail: const Text('finadmin@spsh.lk'),
+          currentAccountPicture: CircleAvatar(
+            child: ClipOval(
+              child: Image.asset('assets/avatarpic.jpg'),
+            ),
+          ),
+        ),
+        Padding(
+          padding:  EdgeInsets.symmetric(horizontal: _width/19.63636363636364), // 20
+          child: ElevatedButton(onPressed: () {}, child: const Text("Pending Claims")),
+        ),
+        Padding(
+          padding:  EdgeInsets.symmetric(horizontal: _width/19.63636363636364), // 20
+          child: ElevatedButton(onPressed: () {}, child: const Text("Reporting and Analytics")),
+        ),
+        Padding(
+          padding:  EdgeInsets.symmetric(horizontal: _width/19.63636363636364), // 20
+          child: ElevatedButton(onPressed: () {}, child: const Text("My Claims")),
+        ),
+
+      ],
+    ));
   }
 }
