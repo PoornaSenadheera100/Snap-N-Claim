9a0c08a8c908dfe115a4313c2233378d5ffa1377
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-09-26 21:10:47 +0530

    Partially designed finance_admin_expense_configurations_screen.dart

--- snap_n_claim/lib/screens/finance_admin_expense_configurations_screen.dart
+++ snap_n_claim/lib/screens/finance_admin_expense_configurations_screen.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
 
 class FinanceAdminExpenseConfigurationsScreen extends StatefulWidget {
-  const FinanceAdminExpenseConfigurationsScreen(this._width, this._height, {super.key});
+  const FinanceAdminExpenseConfigurationsScreen(this._width, this._height,
+      {super.key});
 
   final double _width;
   final double _height;
 
   @override
-  State<FinanceAdminExpenseConfigurationsScreen> createState() => _FinanceAdminExpenseConfigurationsScreenState();
+  State<FinanceAdminExpenseConfigurationsScreen> createState() =>
+      _FinanceAdminExpenseConfigurationsScreenState();
 }
 
-class _FinanceAdminExpenseConfigurationsScreenState extends State<FinanceAdminExpenseConfigurationsScreen> {
+class _FinanceAdminExpenseConfigurationsScreenState
+    extends State<FinanceAdminExpenseConfigurationsScreen> {
   @override
   Widget build(BuildContext context) {
+    print(widget._width);
+    print(widget._height);
     return Scaffold(
-      appBar: AppBar(title: Text("Expense Configurations"),),
+      appBar: AppBar(
+        title: Text("Expense Configurations"),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                  width: widget._width / 3.2,
+                  child: Center(
+                      child: Text(
+                    "GL Code",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ))),
+              SizedBox(
+                  width: widget._width / 3.2,
+                  child: Center(
+                      child: Text(
+                    "Transaction Limit (Rs.)",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ))),
+              SizedBox(
+                  width: widget._width / 3.2,
+                  child: Center(
+                      child: Text(
+                    "Monthly Limit (Rs.)",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ))),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
