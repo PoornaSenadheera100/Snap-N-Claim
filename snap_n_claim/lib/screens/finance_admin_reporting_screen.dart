4ac61745762a190fd4c675cfb961e8bdee3ef593
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-13 23:01:26 +0530

    Added sample graph

--- snap_n_claim/lib/screens/finance_admin_reporting_screen.dart
+++ snap_n_claim/lib/screens/finance_admin_reporting_screen.dart
@@ -276,6 +276,175 @@ class _FinanceAdminReportingScreenState
     });
   }
 
+  BarTouchData get barTouchData => BarTouchData(
+    enabled: false,
+    touchTooltipData: BarTouchTooltipData(
+      tooltipBgColor: Colors.transparent,
+      tooltipPadding: EdgeInsets.zero,
+      tooltipMargin: 8,
+      getTooltipItem: (
+          BarChartGroupData group,
+          int groupIndex,
+          BarChartRodData rod,
+          int rodIndex,
+          ) {
+        return BarTooltipItem(
+          rod.toY.round().toString(),
+          const TextStyle(
+            color: Color(0xFF50E4FF),
+            fontWeight: FontWeight.bold,
+          ),
+        );
+      },
+    ),
+  );
+
+  Widget getTitles(double value, TitleMeta meta) {
+    final style = TextStyle(
+      color: Color(0xFF2196F3),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = 'Mn';
+        break;
+      case 1:
+        text = 'Te';
+        break;
+      case 2:
+        text = 'Wd';
+        break;
+      case 3:
+        text = 'Tu';
+        break;
+      case 4:
+        text = 'Fr';
+        break;
+      case 5:
+        text = 'St';
+        break;
+      case 6:
+        text = 'Sn';
+        break;
+      default:
+        text = '';
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: Text(text, style: style),
+    );
+  }
+
+  FlTitlesData get titlesData => FlTitlesData(
+    show: true,
+    bottomTitles: AxisTitles(
+      sideTitles: SideTitles(
+        showTitles: true,
+        reservedSize: 30,
+        getTitlesWidget: getTitles,
+      ),
+    ),
+    leftTitles: const AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+    topTitles: const AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+    rightTitles: const AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+  );
+
+  FlBorderData get borderData => FlBorderData(
+    show: false,
+  );
+
+  LinearGradient get _barsGradient => LinearGradient(
+    colors: [
+      Color(0xFF2196F3),
+      Color(0xFF50E4FF),
+    ],
+    begin: Alignment.bottomCenter,
+    end: Alignment.topCenter,
+  );
+
+  List<BarChartGroupData> get barGroups => [
+    BarChartGroupData(
+      x: 0,
+      barRods: [
+        BarChartRodData(
+          toY: 8,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 1,
+      barRods: [
+        BarChartRodData(
+          toY: 10,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 2,
+      barRods: [
+        BarChartRodData(
+          toY: 14,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 3,
+      barRods: [
+        BarChartRodData(
+          toY: 15,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 4,
+      barRods: [
+        BarChartRodData(
+          toY: 13,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 5,
+      barRods: [
+        BarChartRodData(
+          toY: 10,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 6,
+      barRods: [
+        BarChartRodData(
+          toY: 16,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+  ];
+
   Widget _employeeReportTab() {
     return Padding(
       padding: const EdgeInsets.all(10.0),
@@ -562,7 +731,17 @@ class _FinanceAdminReportingScreenState
   }
 
   Widget _expenseReportTab() {
-    return Container();
+    return AspectRatio(aspectRatio: 1.6, child: BarChart(
+      BarChartData(
+        barTouchData: barTouchData,
+        titlesData: titlesData,
+        borderData: borderData,
+        barGroups: barGroups,
+        gridData: const FlGridData(show: false),
+        alignment: BarChartAlignment.spaceAround,
+        maxY: 20,
+      ),
+    ),);
   }
 
   @override
