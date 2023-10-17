f2eed2f87461a64bcd696194efc34e0847f8e3d2
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-17 19:24:01 +0530

    Designed finance_admin_view_claim_status_screen.dart partially

--- snap_n_claim/lib/screens/finance_admin/finance_admin_view_claim_status_screen.dart
+++ snap_n_claim/lib/screens/finance_admin/finance_admin_view_claim_status_screen.dart
@@ -17,10 +17,66 @@ class FinanceAdminViewClaimStatusScreen extends StatefulWidget {
 
 class _FinanceAdminViewClaimStatusScreenState
     extends State<FinanceAdminViewClaimStatusScreen> {
+  List<String> _statusDropdownItems = ["Approved", "Rejected"];
+  String _statusDropdownValue = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Claim Report"),),
+      appBar: AppBar(
+        title: Text("Claim Report"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 70,
+              color: Color(0xFFD7D7D7),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: Text("Search"),
+                  ),
+                  Expanded(
+                      child: SizedBox(
+                          child: TextField(
+                    decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Emp No",),
+                  ))),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: Text("Filter"),
+                  ),
+                  DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                        value: _statusDropdownValue.isNotEmpty
+                            ? _statusDropdownValue
+                            : null,
+                        hint: const Text("Status"),
+                        items: _statusDropdownItems
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: TextStyle(
+                                  fontSize: widget._width / 26.18181818181818),
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _statusDropdownValue = newValue!;
+                          });
+                        }),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
