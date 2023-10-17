088122137577e84a5f3a0e5cedf4ffedb538003d
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-17 15:07:57 +0530

    Implemented employee model

---
+++ snap_n_claim/lib/models/employee.dart
@@ -0,0 +1,14 @@
+class Employee{
+  String empNo;
+  String name;
+  String department;
+  String email;
+  String empGrade;
+  String empType;
+  String firstLogin;
+  String password;
+  String phone;
+
+  Employee(this.empNo, this.name, this.department, this.email, this.empGrade,
+      this.empType, this.firstLogin, this.password, this.phone);
+}
