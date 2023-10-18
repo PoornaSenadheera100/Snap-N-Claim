3035502842b9091763479dab57e5843757cf28ed
Sathira Wijeratne <swije27@gmail.com>
2023-10-18 23:25:59 +0530

    implemented request model

---
+++ snap_n_claim/lib/models/request.dart
@@ -0,0 +1,26 @@
+class Request {
+  String category;
+  String claimNo;
+  String date;
+  String department;
+  String empName;
+  String empNo;
+  List<Map<String, dynamic>> lineItems;
+  String paymentStatus;
+  String rejectReason;
+  String status;
+  double total;
+
+  Request(
+      this.category,
+      this.claimNo,
+      this.date,
+      this.department,
+      this.empName,
+      this.empNo,
+      this.lineItems,
+      this.paymentStatus,
+      this.rejectReason,
+      this.status,
+      this.total);
+}
