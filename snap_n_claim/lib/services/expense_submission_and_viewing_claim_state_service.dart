347e8c4d998cc2448b70473437cd95fe526a478c
Sathira Wijeratne <swije27@gmail.com>
2023-10-18 23:26:33 +0530

    implemented methods to get requests

--- snap_n_claim/lib/services/expense_submission_and_viewing_claim_state_service.dart
+++ snap_n_claim/lib/services/expense_submission_and_viewing_claim_state_service.dart
@@ -10,4 +10,48 @@ final CollectionReference requestCollectionReference =
 final CollectionReference employeeCollectionReference =
     _firestore.collection("Employee");
 
-class ExpenseSubmissionAndViewingClaimStateService {}
+class ExpenseSubmissionAndViewingClaimStateService {
+    // static Future<List<Map<String, dynamic>>> getRequests() async{
+    //     List<Map<String, dynamic>> requestList = [];
+    //     QuerySnapshot list = await requestCollectionReference.get();
+    //     for( QueryDocumentSnapshot doc in list.docs){
+    //         Map<String,dynamic> data = {};
+    //         data["claimNo"] = doc["claimNo"];
+    //         data["category"] = doc["category"];
+    //         data["date"] = doc["date"];
+    //         data["department"] = doc["department"];
+    //         data["empName"] = doc["empName"];
+    //         data["empNo"] = doc["empNo"];
+    //         data["lineItems"] = doc["lineItems"];
+    //         data["paymentStatus"] = doc["paymentStatus"];
+    //         data["rejectReason"] = doc["rejectReason"];
+    //         data["status"] = doc["status"];
+    //         data["total"] = doc["total"];
+    //
+    //         requestList.add(data);
+    //     }
+    //
+    //     return requestList;
+    // }
+
+    static Stream<QuerySnapshot> getAllRequests() {
+        return requestCollectionReference.snapshots();
+    }
+
+    static Stream<QuerySnapshot> getPendingRequests() {
+        return requestCollectionReference.where("status", isEqualTo: "Pending").snapshots();
+    }
+
+    static Stream<QuerySnapshot> getApprovedRequests() {
+        return requestCollectionReference.where("status", isEqualTo: "Approved").snapshots();
+    }
+
+    static Stream<QuerySnapshot> getRejectedRequests() {
+        return requestCollectionReference.where("status", isEqualTo: "Rejected").snapshots();
+    }
+
+    static Stream<QuerySnapshot> getDraftRequests() {
+        return requestCollectionReference.where("status", isEqualTo: "Draft").snapshots();
+    }
+}
+
