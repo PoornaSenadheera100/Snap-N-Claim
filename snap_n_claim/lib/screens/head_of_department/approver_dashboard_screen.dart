748c15b3d23a1057eaf181b70f914e8c33a9b028
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-19 15:14:29 +0530

    Fixed render issue

--- snap_n_claim/lib/screens/head_of_department/approver_dashboard_screen.dart
+++ snap_n_claim/lib/screens/head_of_department/approver_dashboard_screen.dart
@@ -6,7 +6,6 @@ import '../../services/expense_approval_process_and_sla_calculation_service.dart
 import 'approver_navbar_widget.dart';
 import 'approver_viewclaimindetail_screen.dart';
 
-
 class ApproverDashboardScreen extends StatefulWidget {
   const ApproverDashboardScreen(this._width, this._height, this._user,
       {Key? key})
@@ -25,17 +24,19 @@ class ApproverDashboardScreen extends StatefulWidget {
 List<String> options = ['Pending', 'Rejected', 'Approved', 'Completed'];
 
 class _ApproverDashboardScreenState extends State<ApproverDashboardScreen> {
-
-  late Stream <QuerySnapshot> PendingClaims;
+  late Stream<QuerySnapshot> PendingClaims;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          centerTitle: true, title: Text("Approver Screen")),
-      drawer: ApproverNavBar(widget._user),
-    body: Column(
-      children:[radioButtonGroup(),LoadPurchaseRequests()],));
+        appBar: AppBar(centerTitle: true, title: Text("Approver Screen")),
+        drawer: ApproverNavBar(widget._user),
+        body: Column(
+          children: [
+            radioButtonGroup(),
+            LoadPurchaseRequests(),
+          ],
+        ));
   }
 
   void initState() {
@@ -46,23 +47,20 @@ class _ApproverDashboardScreenState extends State<ApproverDashboardScreen> {
 
   String currentOption = options[0];
 
-
 //Method : Get all pending requests
-  void getPendingPurchaseOrders()  {
-    PendingClaims = ExpenseApprovalProcessAndSlaCalculationService.getPendingClaims(
-        widget._user.department);
+  void getPendingPurchaseOrders() {
+    PendingClaims =
+        ExpenseApprovalProcessAndSlaCalculationService.getPendingClaims(
+            widget._user.department);
   }
 
-
   void filter(String option) {
-
     /*setState(() {
     purchaseorders2 = purchaseorders1
         .where((element) => element["status"] == option)
         .toList();
   });*/
   }
-
 
   Widget radioButtonGroup() {
     return Container(
@@ -94,134 +92,129 @@ class _ApproverDashboardScreenState extends State<ApproverDashboardScreen> {
     );
   }
 
-  Widget LoadPurchaseRequests() =>
-      StreamBuilder(
-          stream: PendingClaims,
-          builder: (BuildContext context,
-              AsyncSnapshot <QuerySnapshot> snapShot) {
-            if (snapShot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator.adaptive();
-            }
-            else if (snapShot.data!.docs.length > 0) {
-              return ListView(children: snapShot.data!.docs.map((e) =>
-                  GestureDetector(
-                    onTap: ()  {
-                      // Navigate to the my claims page when the item is tapped
-                      Navigator.of(context).pop(); // Close the drawer
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => ViewClaimInDetail(
-                              widget._width, widget._height, widget._user),
-                        ),
-                      );
-                    } ,
-                    key: UniqueKey(),
-                    child: Card(
-                      elevation: 5,
-                      shadowColor: Colors.black,
-                      color: e["status"] == "Rejected"
-                          ? Color(0xFFDA8383)
-                          : e["status"] == "Completed"
-                          ? Color(0xFF66DE87)
-                          : Color(0xFFC8E7F2),
-                      child: SizedBox(
-                        width: 400,
-                        height: 160,
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            children: [
-                              const SizedBox(
-                                height: 10,
+  Widget LoadPurchaseRequests() => StreamBuilder(
+      stream: PendingClaims,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapShot) {
+        if (snapShot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator.adaptive();
+        } else if (snapShot.data!.docs.length > 0) {
+          return Container(
+            height: widget._height /1.247012987012987,
+            child: ListView(
+                children: snapShot.data!.docs
+                    .map((e) => GestureDetector(
+                          onTap: () {
+                            // Navigate to the my claims page when the item is tapped
+                            Navigator.of(context).pop(); // Close the drawer
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => ViewClaimInDetail(
+                                    widget._width, widget._height, widget._user),
                               ),
-                              Text(
-                                'PO Number : ',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500,
+                            );
+                          },
+                          key: UniqueKey(),
+                          child: Card(
+                            elevation: 5,
+                            shadowColor: Colors.black,
+                            color: e["status"] == "Rejected"
+                                ? Color(0xFFDA8383)
+                                : e["status"] == "Completed"
+                                    ? Color(0xFF66DE87)
+                                    : Color(0xFFC8E7F2),
+                            child: SizedBox(
+                              width: 400,
+                              height: 160,
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Column(
+                                  children: [
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      'PO Number : ',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    // Text(
+                                    //   'DO Number : ${e["deliveryId"]}',
+                                    //   style: TextStyle(
+                                    //     fontSize: 15,
+                                    //     color: Colors.black,
+                                    //     fontWeight: FontWeight.w500,
+                                    //   ),
+                                    // ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Item : ' '',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Quantity : ' '',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Amount : Rs.10,000',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Date : ${e["date"].toString().substring(0, 10)}',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                  ],
                                 ),
                               ),
-                              // Text(
-                              //   'DO Number : ${e["deliveryId"]}',
-                              //   style: TextStyle(
-                              //     fontSize: 15,
-                              //     color: Colors.black,
-                              //     fontWeight: FontWeight.w500,
-                              //   ),
-                              // ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment
-                                    .spaceBetween,
-                                children: [
-                                  Text(
-                                    'Item : ''',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  Text(
-                                    'Quantity : ''',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment
-                                    .spaceBetween,
-                                children: [
-                                  Text(
-                                    'Amount : Rs.10,000',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  Text(
-                                    'Date : ${e["date"].toString().substring(
-                                        0, 10)}',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ),
-                    ),
-                  )
-              ).toList()
-              );
-            }
-            else {
-              return
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/noPendingItems.png'),
-                      Text('You have no pending items'),
-                    ],
-                  ),
-                );
-
-            }
-          }
-      );
+                        ))
+                    .toList()),
+          );
+        } else {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/noPendingItems.png'),
+                Text('You have no pending items'),
+              ],
+            ),
+          );
+        }
+      });
 }
