3ee541b523143598d7d3e30b266f072a0205a4aa
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-19 20:02:48 +0530

    Created fin admin pending claims screen

--- snap_n_claim/lib/screens/finance_admin/finance_admin_home_screen.dart
+++ snap_n_claim/lib/screens/finance_admin/finance_admin_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:snap_n_claim/models/employee.dart';
+import 'package:snap_n_claim/screens/finance_admin/finance_admin_claims_payment_screen.dart';
 import 'package:snap_n_claim/screens/finance_admin/finance_admin_menu_drawer.dart';
 import 'package:snap_n_claim/services/budget_allocation_and_reporting_service.dart';
 
@@ -56,18 +57,120 @@ class _FinanceAdminHomeScreenState extends State<FinanceAdminHomeScreen> {
               ),
             );
           } else if (snapshot.data!.docs.length > 0) {
-            return SingleChildScrollView(
-              child: Column(
-                children: snapshot.data!.docs
-                    .map((e) => Text(e["date"].toDate().toString()))
-                    .toList(),
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: SingleChildScrollView(
+                child: SizedBox(
+                  width: widget._width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: snapshot.data!.docs
+                        .map((e) => GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        FinanceAdminClaimsPaymentScreen(
+                                            widget._width,
+                                            widget._height,
+                                            widget.user)));
+                              },
+                              key: UniqueKey(),
+                              child: Card(
+                                elevation: 5,
+                                shadowColor: Colors.black,
+                                color: Color(0x81A2C5FF),
+                                child: SizedBox(
+                                  width: 350,
+                                  height: 105,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(20.0),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              DateTime.parse(e["date"]
+                                                      .toDate()
+                                                      .toString())
+                                                  .toString()
+                                                  .substring(0, 10),
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            Text(
+                                              'Rs.${e["total"].toStringAsFixed(2)}',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              e["claimNo"],
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            Text(
+                                              'Status : ${e["status"]}',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              '${e["category"]}',
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            Text(
+                                              e["empName"],
+                                              style: TextStyle(
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ))
+                        .toList(),
+                  ),
+                ),
               ),
             );
           } else {
-            return Center(child: Column(
+            return Center(
+                child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.tag_faces_rounded, size: 100, color: Colors.green,),
+                Icon(
+                  Icons.tag_faces_rounded,
+                  size: 100,
+                  color: Colors.green,
+                ),
                 Text("No Pending Claims"),
               ],
             ));
