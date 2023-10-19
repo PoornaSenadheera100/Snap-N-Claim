b1068fa87b7b1ad02ffe08b3338428b4a84e3be4
SanuthiVihansa <subasofts@gmail.com>
2023-10-19 07:19:30 +0530

    commit approval screen

---
+++ snap_n_claim/lib/screens/head_of_department/approver_approvalhistory_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class approvalHistory extends StatefulWidget {
+  const approvalHistory({Key? key}) : super(key: key);
+
+  @override
+  State<approvalHistory> createState() => _approvalHistoryState();
+}
+
+class _approvalHistoryState extends State<approvalHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+//This Screen is kind of report
