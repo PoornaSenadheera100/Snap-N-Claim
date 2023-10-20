0189231b6da98bcd39a311a8229d50e5cf18dd85
Sathira Wijeratne <swije27@gmail.com>
2023-10-20 18:07:53 +0530

    added backed methods

--- snap_n_claim/lib/services/expense_submission_and_viewing_claim_state_service.dart
+++ snap_n_claim/lib/services/expense_submission_and_viewing_claim_state_service.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../models/response.dart';
+
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference expenseCollectionReference =
     _firestore.collection("Expense");
@@ -11,47 +13,205 @@ final CollectionReference employeeCollectionReference =
     _firestore.collection("Employee");
 
 class ExpenseSubmissionAndViewingClaimStateService {
-    // static Future<List<Map<String, dynamic>>> getRequests() async{
-    //     List<Map<String, dynamic>> requestList = [];
-    //     QuerySnapshot list = await requestCollectionReference.get();
-    //     for( QueryDocumentSnapshot doc in list.docs){
-    //         Map<String,dynamic> data = {};
-    //         data["claimNo"] = doc["claimNo"];
-    //         data["category"] = doc["category"];
-    //         data["date"] = doc["date"];
-    //         data["department"] = doc["department"];
-    //         data["empName"] = doc["empName"];
-    //         data["empNo"] = doc["empNo"];
-    //         data["lineItems"] = doc["lineItems"];
-    //         data["paymentStatus"] = doc["paymentStatus"];
-    //         data["rejectReason"] = doc["rejectReason"];
-    //         data["status"] = doc["status"];
-    //         data["total"] = doc["total"];
-    //
-    //         requestList.add(data);
-    //     }
-    //
-    //     return requestList;
-    // }
 
-    static Stream<QuerySnapshot> getAllRequests() {
-        return requestCollectionReference.snapshots();
+  static Stream<QuerySnapshot> getAllRequestsByempNo(String empNo) {
+    return requestCollectionReference
+        .where('empNo', isEqualTo: empNo)
+        .snapshots();
+  }
+
+  static Future<QuerySnapshot<Object?>> getLatestClaimNo() {
+    return requestCollectionReference
+        .orderBy('claimNo', descending: true)
+        .limit(1)
+        .get();
+  }
+
+  static Stream<QuerySnapshot> getPendingRequests(String empNo) {
+    return requestCollectionReference
+        .where("status", isEqualTo: "Pending")
+        .where('empNo', isEqualTo: empNo)
+        .snapshots();
+  }
+
+  static Stream<QuerySnapshot> getApprovedRequests(String empNo) {
+    return requestCollectionReference
+        .where("status", isEqualTo: "Approved")
+        .where('empNo', isEqualTo: empNo)
+        .snapshots();
+  }
+
+  static Stream<QuerySnapshot> getRejectedRequests(String empNo) {
+    return requestCollectionReference
+        .where("status", isEqualTo: "Rejected")
+        .where('empNo', isEqualTo: empNo)
+        .snapshots();
+  }
+
+  static Stream<QuerySnapshot> getDraftRequests(String empNo) {
+    return requestCollectionReference
+        .where("status", isEqualTo: "Draft")
+        .where('empNo', isEqualTo: empNo)
+        .snapshots();
+  }
+
+  static Stream<QuerySnapshot> getExpensesByClaimNo(String claimNo) {
+    return requestCollectionReference
+        .where("claimNo", isEqualTo: claimNo)
+        .where('status', isEqualTo: 'Draft')
+        .snapshots();
+  }
+
+  static Future<Response> addRequest(Map<String, dynamic> request) async {
+    Response response = Response();
+
+    await requestCollectionReference.doc().set(request).whenComplete(() {
+      response.code = 200;
+      response.message = "Line Item added";
+    }).catchError((e) {
+      response.code = 500;
+      response.message = e;
+    });
+
+    return response;
+  }
+
+  static Future<Response> updateRequest(Map<String, dynamic> request,
+      String claimNo) async {
+    Response response = Response();
+
+    try {
+      QuerySnapshot querySnapshot = await requestCollectionReference
+          .where("claimNo", isEqualTo: claimNo)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        QueryDocumentSnapshot document = querySnapshot.docs[0];
+
+        await document.reference.update(request);
+        response.code = 200;
+        response.message = "Line item added";
+      } else {
+        response.code = 404;
+        response.message =
+        "Claim request not found with the specified claim number";
+      }
+    } catch (e) {
+      response.code = 500;
+      response.message = e.toString();
     }
 
-    static Stream<QuerySnapshot> getPendingRequests() {
-        return requestCollectionReference.where("status", isEqualTo: "Pending").snapshots();
+    return response;
+  }
+
+  static Future<Response> deleteLineItem(String claimNo,
+      String invoiceNo) async {
+    Response response = Response();
+
+    try {
+      QuerySnapshot querySnapshot = await requestCollectionReference
+          .where("claimNo", isEqualTo: claimNo)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        QueryDocumentSnapshot document = querySnapshot.docs[0];
+
+        List l = document["lineItems"];
+
+        l.removeWhere((element) => element["invoiceNo"] == invoiceNo);
+
+        Map<String, dynamic> newData = {
+          "category": document["category"],
+          "claimNo": document["claimNo"],
+          "date": document["date"],
+          "department": document["department"],
+          "empName": document["empName"],
+          "empNo": document["empNo"],
+          "lineItems": l,
+          "paymentStatus": document["paymentStatus"],
+          "rejectReason": document["rejectReason"],
+          "status": document["status"],
+          "total": document["total"],
+        };
+
+        await requestCollectionReference.doc(document.id).update(newData);
+        response.code = 200;
+        response.message = "Line item removed";
+      } else {
+        response.code = 404;
+        response.message =
+        "Claim request not found with the specified claim number";
+      }
+    } catch (e) {
+      response.code = 500;
+      response.message = e.toString();
     }
 
-    static Stream<QuerySnapshot> getApprovedRequests() {
-        return requestCollectionReference.where("status", isEqualTo: "Approved").snapshots();
+    return response;
+  }
+
+  static Future<Response> deleteClaim(String claimNo) async {
+    Response response = Response();
+
+    try {
+      QuerySnapshot querySnapshot = await requestCollectionReference
+          .where("claimNo", isEqualTo: claimNo)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        QueryDocumentSnapshot document = querySnapshot.docs[0];
+
+        await document.reference.delete();
+
+        response.code = 200;
+        response.message = "Claim deleted";
+      }
+    } catch (e) {
+      response.code = 500;
+      response.message = e.toString();
     }
 
-    static Stream<QuerySnapshot> getRejectedRequests() {
-        return requestCollectionReference.where("status", isEqualTo: "Rejected").snapshots();
+    return response;
+  }
+
+  static Future<Response> updateClaimStatus(String claimNo) async{
+    Response response = Response();
+
+    try{
+      QuerySnapshot querySnapshot = await requestCollectionReference
+          .where("claimNo", isEqualTo: claimNo)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        QueryDocumentSnapshot document = querySnapshot.docs[0];
+
+        Map<String, dynamic> newData = {
+          "category": document["category"],
+          "claimNo": document["claimNo"],
+          "date": document["date"],
+          "department": document["department"],
+          "empName": document["empName"],
+          "empNo": document["empNo"],
+          "lineItems": document["lineItems"],
+          "paymentStatus": document["paymentStatus"],
+          "rejectReason": document["rejectReason"],
+          "status": "Pending",
+          "total": document["total"],
+        };
+
+        await requestCollectionReference.doc(document.id).update(newData);
+        response.code = 200;
+        response.message = "Claim status updated";
+      } else {
+        response.code = 404;
+        response.message =
+        "Claim request not found with the specified claim number";
+      }
+    } catch (e) {
+      response.code = 500;
+      response.message = e.toString();
     }
 
-    static Stream<QuerySnapshot> getDraftRequests() {
-        return requestCollectionReference.where("status", isEqualTo: "Draft").snapshots();
-    }
+    return response;
+  }
 }
-
