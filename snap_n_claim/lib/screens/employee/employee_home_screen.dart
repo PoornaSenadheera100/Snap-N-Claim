aa252537ff05e79564cbd1b1c9fb1f0d0382ba7d
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-22 14:16:56 +0530

    Added sample code for sendgrid

--- snap_n_claim/lib/screens/employee/employee_home_screen.dart
+++ snap_n_claim/lib/screens/employee/employee_home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sendgrid_mailer/sendgrid_mailer.dart';
 
 import '../../models/employee.dart';
 
@@ -9,6 +10,24 @@ class EmployeeHomeScreen extends StatelessWidget {
   final double _height;
   final Employee _user;
 
+  // TODO - Remove
+  void sendMail() async {
+    final mailer = Mailer(
+        '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');
+    final toAddress = Address('poornasenadheeraonline@gmail.com');
+    final fromAddress = Address('poornasenadheera100@gmail.com');
+    final content = Content('text/plain', 'Hello World!');
+    final subject = 'Hello Subject!';
+    final personalization = Personalization([toAddress]);
+
+    final email = Email([personalization], fromAddress, subject, content: [content]);
+    mailer.send(email).then((result) {
+      print(result.toString());
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +37,11 @@ class EmployeeHomeScreen extends StatelessWidget {
       body: const Center(
         child: Text("Welcome"),
       ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            sendMail();
+          },
+          child: const Icon(Icons.add)),
     );
   }
 }
