3ac5140318492025933020f010765bc6e5c274a3
Sathira Wijeratne <swije27@gmail.com>
2023-10-25 22:29:18 +0530

    improved UI

--- snap_n_claim/lib/screens/head_of_department/approver_dashboard_screen.dart
+++ snap_n_claim/lib/screens/head_of_department/approver_dashboard_screen.dart
@@ -25,6 +25,8 @@ List<String> options = ['Pending', 'Rejected', 'Approved', 'Completed'];
 
 class _ApproverDashboardScreenState extends State<ApproverDashboardScreen> {
   late Stream<QuerySnapshot> _pendingClaims;
+  final double deviceWidth = 392.72727272727275;
+  final double deviceHeight = 783.2727272727273;
 
   @override
   Widget build(BuildContext context) {
@@ -100,7 +102,7 @@ class _ApproverDashboardScreenState extends State<ApproverDashboardScreen> {
               Text(
                 option,
                 style: TextStyle(
-                  fontSize: widget._width / 39.27272727272727,
+                  fontSize: widget._width / 28,
                 ),
               ),
             ],
@@ -120,117 +122,120 @@ class _ApproverDashboardScreenState extends State<ApproverDashboardScreen> {
             height: widget._height / 1.247012987012987,
             child: ListView(
                 children: snapShot.data!.docs
-                    .map((e) => GestureDetector(
-                          onTap: () {
-                            // Navigate to the my claims page when the item is tapped
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => ViewClaimInDetail(
-                                    widget._width,
-                                    widget._height,
-                                    widget._user,
-                                    e),
-                              ),
-                            );
-                          },
-                          key: UniqueKey(),
-                          child: Card(
-                            elevation: 5,
-                            shadowColor: Colors.black,
-                            color: e["status"] == "Rejected"
-                                ? const Color(0xFFDA8383)
-                                : (e["status"] == "Approved" &&
-                                        e["paymentStatus"] == "Pending")
-                                    ? const Color(0xFF948BB4)
-                                    : (e["status"] == "Approved" &&
-                                            e["paymentStatus"] == "Paid")
-                                        ? const Color(0xFF66DE87)
-                                        : const Color(0xFFD6D156),
-                            child: SizedBox(
-                              width: widget._width / 0.9818181818181818,
-                              height: widget._height / 5.018181818181818,
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                  vertical: widget._height / 40.14545454545455,
-                                  horizontal: widget._width / 19.63636363636364,
+                    .map((e) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                            onTap: () {
+                              // Navigate to the my claims page when the item is tapped
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => ViewClaimInDetail(
+                                      widget._width,
+                                      widget._height,
+                                      widget._user,
+                                      e),
                                 ),
-                                child: Column(
-                                  children: [
-                                    SizedBox(
-                                      height:
-                                          widget._height / 80.29090909090909,
-                                    ),
-                                    Text(
-                                      'Date : ${e["date"].toDate().toString().substring(0, 10)}',
-                                      style: TextStyle(
-                                        fontSize:
-                                            widget._width / 26.18181818181818,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w500,
+                              );
+                            },
+                            key: UniqueKey(),
+                            child: Card(
+                              elevation: 5,
+                              shadowColor: Colors.black,
+                              color: e["status"] == "Rejected"
+                                  ? const Color(0xFFDA8383)
+                                  : (e["status"] == "Approved" &&
+                                          e["paymentStatus"] == "Pending")
+                                      ? const Color(0xFF948BB4)
+                                      : (e["status"] == "Approved" &&
+                                              e["paymentStatus"] == "Paid")
+                                          ? const Color(0xFF66DE87)
+                                          : const Color(0xFFD6D156),
+                              child: SizedBox(
+                                width: widget._width / (deviceWidth / 400),
+                                height: widget._height / (deviceHeight / 135),
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: widget._height / 40.14545454545455,
+                                    horizontal: widget._width / 19.63636363636364,
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      SizedBox(
+                                        height:
+                                            widget._height / 80.29090909090909,
                                       ),
-                                    ),
-                                    SizedBox(
-                                      height:
-                                          widget._height / 80.29090909090909,
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          '${e["empName"]}',
-                                          style: TextStyle(
-                                            fontSize: widget._width /
-                                                26.18181818181818,
-                                            color: Colors.black,
-                                          ),
+                                      Text(
+                                        'Date : ${e["date"].toDate().toString().substring(0, 10)}',
+                                        style: TextStyle(
+                                          fontSize:
+                                              widget._width / 26.18181818181818,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w500,
                                         ),
-                                        Text(
-                                          '${e["claimNo"]}',
-                                          style: TextStyle(
-                                            fontSize: widget._width /
-                                                26.18181818181818,
-                                            color: Colors.black,
+                                      ),
+                                      SizedBox(
+                                        height:
+                                            widget._height / 80.29090909090909,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            '${e["empName"]}',
+                                            style: TextStyle(
+                                              fontSize: widget._width /
+                                                  26.18181818181818,
+                                              color: Colors.black,
+                                            ),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height:
-                                          widget._height / 80.29090909090909,
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          'Rs.${e["total"].toStringAsFixed(2)}',
-                                          style: TextStyle(
-                                            fontSize: widget._width /
-                                                26.18181818181818,
-                                            color: Colors.black,
+                                          Text(
+                                            '${e["claimNo"]}',
+                                            style: TextStyle(
+                                              fontSize: widget._width /
+                                                  26.18181818181818,
+                                              color: Colors.black,
+                                            ),
                                           ),
-                                        ),
-                                        Text(
-                                          '${e["category"]}',
-                                          style: TextStyle(
-                                            fontSize: widget._width /
-                                                26.18181818181818,
-                                            color: Colors.black,
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height:
+                                            widget._height / 80.29090909090909,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            'Rs.${e["total"].toStringAsFixed(2)}',
+                                            style: TextStyle(
+                                              fontSize: widget._width /
+                                                  26.18181818181818,
+                                              color: Colors.black,
+                                            ),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height:
-                                          widget._height / 80.29090909090909,
-                                    ),
-                                  ],
+                                          Text(
+                                            '${e["category"]}',
+                                            style: TextStyle(
+                                              fontSize: widget._width /
+                                                  26.18181818181818,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height:
+                                            widget._height / 80.29090909090909,
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ))
+                    ))
                     .toList()),
           );
         } else {
